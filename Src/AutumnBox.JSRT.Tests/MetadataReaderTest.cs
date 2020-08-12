3ab7a2e6d95617bfa72fa817215cf4fb3e5c1209
zsh2401 <zsh2401@163.com>
2020-08-13 05:25:39 +0800

    Create MetadataReader and test code about it.

---
+++ Src/AutumnBox.JSRT.Tests/MetadataReaderTest.cs
@@ -0,0 +1,96 @@
+﻿/*
+
+* ==============================================================================
+*
+* Filename: HeaderReaderTest
+* Description: 
+*
+* Version: 1.0
+* Created: 2020/8/13 5:12:37
+* Compiler: Visual Studio 2019
+*
+* Author: zsh2401
+*
+* ==============================================================================
+*/
+using AutumnBox.JSRT.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutumnBox.JSRT.Tests
+{
+    [TestClass]
+    public class MetadataReaderTest
+    {
+        [TestMethod]
+        public void BasicParse()
+        {
+            string header =
+                "// @name test\n" +
+                "// @description test desc\n" +
+                "// @url https://www.atmb.top\n" +
+                "// @opensource https://github.com/zsh2401/AutumnBox \n";
+            var reader = new MetadataReader(header);
+            Assert.AreEqual("test", reader["name"]);
+            Assert.AreEqual("test desc", reader["description"]);
+            Assert.AreEqual("https://www.atmb.top", reader["url"]);
+            Assert.AreEqual("https://github.com/zsh2401/AutumnBox ", reader["opensource"]);
+        }
+
+        [TestMethod]
+        public void ParseWithNoise()
+        {
+            string header =
+                "// sadsadasasad \n" +
+                "// @name test\n" +
+                "// =====AutumnBox Script\n" +
+                "// @description test desc\n" +
+                "// @url https://www.atmb.top\n" +
+                "// @opensource https://github.com/zsh2401/AutumnBox \n";
+            var reader = new MetadataReader(header);
+            Assert.AreEqual("test", reader["name"]);
+            Assert.AreEqual("test desc", reader["description"]);
+            Assert.AreEqual("https://www.atmb.top", reader["url"]);
+            Assert.AreEqual("https://github.com/zsh2401/AutumnBox ", reader["opensource"]);
+        }
+
+        [TestMethod]
+        public void InvaildStatement()
+        {
+            string header =
+            "// sadsadasasad \n" +
+            "//@name test\n" +
+            "// @description test desc\n" +
+            "// @url https://www.atmb.top\n" +
+            "// @opensource https://github.com/zsh2401/AutumnBox \n";
+
+            var reader = new MetadataReader(header);
+            Assert.AreEqual("test desc", reader["description"]);
+            Assert.AreEqual("https://www.atmb.top", reader["url"]);
+            Assert.AreEqual("https://github.com/zsh2401/AutumnBox ", reader["opensource"]);
+            Assert.ThrowsException<KeyNotFoundException>(() => _ = reader["name"]);
+        }
+
+        [TestMethod]
+        public void WithRealJavaScript()
+        {
+            string header =
+                "// @name test\n" +
+                "// @description test desc\n" +
+                "// @url https://www.atmb.top\n" +
+                "// @opensource https://github.com/zsh2401/AutumnBox \n" +
+                "function sayHello(){" +
+                "   console.log(\"Hello World!\");" +
+                "}" +
+                "sayHello()";
+            var reader = new MetadataReader(header);
+
+            Assert.AreEqual("test", reader["name"]);
+            Assert.AreEqual("test desc", reader["description"]);
+            Assert.AreEqual("https://www.atmb.top", reader["url"]);
+            Assert.AreEqual("https://github.com/zsh2401/AutumnBox ", reader["opensource"]);
+        }
+    }
+}
---
+++ Src/AutumnBox.JSRT/Util/MetadataReader.cs
@@ -0,0 +1,47 @@
+﻿/*
+
+* ==============================================================================
+*
+* Filename: HeaderParser
+* Description: 
+*
+* Version: 1.0
+* Created: 2020/8/13 5:05:18
+* Compiler: Visual Studio 2019
+*
+* Author: zsh2401
+*
+* ==============================================================================
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AutumnBox.JSRT.Util
+{
+    public sealed class MetadataReader
+    {
+        private readonly Dictionary<string, string> KV;
+        public string this[string key]
+        {
+            get
+            {
+                return KV[key];
+            }
+        }
+        private readonly static Regex headerMetadataRegex =
+            new Regex(@"^//\s@(?<key>[\w|\d]+)\s+(?<value>.+)$", RegexOptions.Compiled | RegexOptions.Multiline);
+        public MetadataReader(string jsScript)
+        {
+            KV = new Dictionary<string, string>();
+            foreach (Match match in headerMetadataRegex.Matches(jsScript))
+            {
+                if (match.Success)
+                {
+                    KV.Add(match.Result("${key}"), match.Result("${value}"));
+                }
+            }
+        }
+    }
+}
